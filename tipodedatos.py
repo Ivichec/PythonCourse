78db2adfcacc7a587c368e45c9fe1462f04bfec0
Ivichec <ivanchecag2003@gmail.com>
2024-04-17 19:53:26 +0200

    Strings

---
+++ tipodedatos.py
@@ -0,0 +1,9 @@
+frase = """Hola,
+Me queria presentar:
+Me llamo Pablo"""
+
+nombre = "Iván"
+bienvenida = f"Hola {nombre} como estas hoy?"
+print(bienvenida)
+#Puedes buscar en textos patrones segun indiques si estan o si no estan devuelve true o false.
+#print("Hola" in bienvenida)
