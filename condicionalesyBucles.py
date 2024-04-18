e070b849aa7f25ef10f58c3d03373c772cd2e092
Ivichec <ivanchecag2003@gmail.com>
2024-04-18 12:52:54 +0200

    condicionalesyBucles.

---
+++ condicionalesyBucles.py
@@ -0,0 +1,21 @@
+#if else
+edad = 19
+if edad <= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+#else if
+ingresoMensual = 10000
+if ingresoMensual > 10000:
+    print("Eres rico")
+elif ingresoMensual < 10000:
+    print("Eres pobre")
+else:
+    print("Ganas 10000")
+#for
+
+#switch
+
+#while
+
+#
